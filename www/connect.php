9f64768d5b56d3affc6bbc2befcd1c1b7b733f81
Bernhard Heinloth <heinloth@cs.fau.de>
2020-09-24 16:02:47 +0200

    Adjust licence

--- www/connect.php
+++ www/connect.php
@@ -1,4 +1,21 @@
 <?php
+/*  Remote IDE via Xpra
+ *  Copyright (C) 2020 Bernhard Heinloth and Felix Kraus
+ *  Friedrich-Alexander-Universität Erlangen-Nürnberg (FAU).
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 $debug = false;
 error_reporting($debug ? E_ALL : 0);
 
